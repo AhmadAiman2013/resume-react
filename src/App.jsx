65419d428df40480ba1a7ff4f5caec440901f616
Aiman <ahmadaimanalias@gmail.com>
2023-05-30 10:54:58 +0800

    add experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
 
+function Experience() {
+  return (
+    <>
+      <h3>Software student</h3>
+      <h4>Sigma Lab Sdn Bhd</h4>
+    </>
+  )
+}
+
 function App() {
   
   return (
     <>
       <h1>Ahmad Aiman</h1>
       <h2>Software student</h2>
+      <Experience />
     </>
   )
 }
