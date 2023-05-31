26f814955e00dfc2c239e3306e6ef56e95f9b023
Aiman <ahmadaimanalias@gmail.com>
2023-05-31 09:07:34 +0800

    add accordian

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,71 @@
 
 
+// function Experience() {
+//   return (
+//     <>
+//       <h3>Software student</h3>
+//       <h4>Sigma Lab Sdn Bhd</h4>
+//     </>
+//   )
+// }
+
+// function App() {
+  
+//   return (
+//     <div className='text-center py-5'>
+//       <h1>Ahmad Aiman</h1>
+//       <h2>Software student</h2>
+//       <Experience />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 function Experience() {
   return (
     <>
-      <h3>Software student</h3>
-      <h4>Sigma Lab Sdn Bhd</h4>
+      <div className="accordion" id="experienceAccordion">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#experienceCollapse"
+              aria-expanded="true"
+              aria-controls="experienceCollapse"
+            >
+              <span className='text-primary'>Software Student</span>
+            </button>
+          </h2>
+          <div
+            id="experienceCollapse"
+            className="accordion-collapse collapse show"
+            aria-labelledby="experienceCollapse"
+            data-bs-parent="#experienceAccordion"
+          >
+            <div className="accordion-body">
+              <h4>Sigma Lab Sdn Bhd</h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 function App() {
-  
   return (
-    <div className='text-center py-5'>
+    <div className='d-flex flex-column justify-content-centre align-items-center py-5'>
       <h1>Ahmad Aiman</h1>
       <h2>Software student</h2>
-      <Experience />
+      <div className="accordion">
+        <Experience />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
